04f7ee6c5da1e13c85545ce921816d3ced577de5
Akash Basavaraju <bmnakash@gmail.com>
2022-08-12 00:27:34 +0530

    featL other api integration with mockaroo

--- src/services/APIService.ts
+++ src/services/APIService.ts
@@ -8,6 +8,7 @@ class APIService {
       mode: "no-cors",
       headers: {
         "Content-Type": "application/json",
+        "X-API-Key": "655a9b40",
       },
       body: JSON.stringify(data),
     });
@@ -15,48 +16,66 @@ class APIService {
     return resp;
   }
 
-  getRegistrations(): Promise<IRegistrationType[]> {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(
-          new Array(20).fill({
-            name: "Akash Basavaraju",
-            age: 27,
-            dob: "12/10/1995",
-            profession: "Engineer",
-            locality: "Bangalore",
-            noOfGuests: 1,
-            address: "bangalore",
-          })
-        );
-      }, 2000);
-    });
+  async getRegistrations(): Promise<IRegistrationType[]> {
+    // return new Promise((resolve) => {
+    //   setTimeout(() => {
+    //     resolve(
+    //       new Array(20).fill({
+    //         name: "Akash Basavaraju",
+    //         age: 27,
+    //         dob: "12/10/1995",
+    //         profession: "Engineer",
+    //         locality: "Bangalore",
+    //         noOfGuests: 1,
+    //         address: "bangalore",
+    //       })
+    //     );
+    //   }, 2000);
+    // });
+
+    const response = await fetch(
+      "https://my.api.mockaroo.com/registrations.json?key=655a9b40",
+      {
+        method: "GET",
+      }
+    );
+    const resp = await response.json();
+    return resp;
   }
 
-  getRegistrationReport(): Promise<IReportsDataType> {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve({
-          ageCompare: [
-            { age: 20, noOfPeople: 40 },
-            { age: 22, noOfPeople: 80 },
-          ],
-          localityCompare: [
-            { locality: "bangalore", noOfPeople: 40 },
-            { locality: "chennai", noOfPeople: 80 },
-          ],
-          guestsCompare: [
-            { group: "0", noOfPeople: 40 },
-            { group: "1", noOfPeople: 80 },
-            { group: "2", noOfPeople: 100 },
-          ],
-          professionCompare: [
-            { profession: "Students", noOfPeople: 40 },
-            { profession: "Professionals", noOfPeople: 80 },
-          ],
-        });
-      }, 2000);
-    });
+  async getRegistrationReport(): Promise<IReportsDataType> {
+    // return new Promise((resolve) => {
+    //   setTimeout(() => {
+    //     resolve({
+    //       ageCompare: [
+    //         { age: 20, noOfPeople: 40 },
+    //         { age: 22, noOfPeople: 80 },
+    //       ],
+    //       localityCompare: [
+    //         { locality: "bangalore", noOfPeople: 40 },
+    //         { locality: "chennai", noOfPeople: 80 },
+    //       ],
+    //       guestsCompare: [
+    //         { group: "0", noOfPeople: 40 },
+    //         { group: "1", noOfPeople: 80 },
+    //         { group: "2", noOfPeople: 100 },
+    //       ],
+    //       professionCompare: [
+    //         { profession: "Students", noOfPeople: 40 },
+    //         { profession: "Professionals", noOfPeople: 80 },
+    //       ],
+    //     });
+    //   }, 2000);
+    // });
+
+    const response = await fetch(
+      "https://my.api.mockaroo.com/reportsdata.json?key=655a9b40",
+      {
+        method: "GET",
+      }
+    );
+    const resp = await response.json();
+    return resp;
   }
 }
 

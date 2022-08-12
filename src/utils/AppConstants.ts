eb399ae8af981a187b3ca06d156fcd68fb2eeab5
Akash Basavaraju <bmnakash@gmail.com>
2022-08-13 01:58:56 +0530

    feat: decrease mobile height limit

--- src/utils/AppConstants.ts
+++ src/utils/AppConstants.ts
@@ -1,6 +1,6 @@
 import { NavigationRoutePathTypes } from "../types/NavigationPathTypes";
 
-export const IS_MOBILE = window.innerWidth <= 800 && window.innerHeight <= 600;
+export const IS_MOBILE = window.innerWidth <= 800 && window.innerHeight <= 700;
 
 export const NAVIGATION_ROUTE_PATHS: NavigationRoutePathTypes = {
   regiform: { path: "/registrationform", name: "Registration Form" },
